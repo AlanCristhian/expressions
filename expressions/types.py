675f5d44604d38be21964049d21da66f1d2e6dbb
Alan Cristhian <alan.cristh@gmail.com>
2014-10-26 16:26:41 -0300

    fix sintax for multidimensional matrix definition and check that the item is of the right type

--- expressions/types.py
+++ expressions/types.py
@@ -4,7 +4,7 @@
 __all__ = ['Natural']
 
 
-class NumberMeta(type):
+class _NumberMeta(type):
     """A metaclass that define mathematical operators for the class."""
     def __init__(cls, name, bases, namespace):
         return super().__init__(name, bases, namespace)
@@ -13,12 +13,13 @@ class NumberMeta(type):
         """Make an N-vector with N = exponent"""
         assert isinstance(exponent, int)
         assert exponent > 0
+
         return cls([0 for i in range(0, exponent)])
 
 
-class _NaturalVector(metaclass=NumberMeta):
+class _NaturalMatrix(metaclass=_NumberMeta):
     """Add `ℕ**n` interface for vector definition. Do that by the 
-    NumberMeta.__pow__() method. E.g.:
+    _NumberMeta.__pow__() method. E.g.:
     >>> v = Natural**3
     >>> v
     Natural([0, 0, 0])
@@ -31,25 +32,28 @@ class _NaturalVector(metaclass=NumberMeta):
     def __getitem__(self, arg):
         return self._array.__getitem__(arg)
 
+    def __setitem__(self, key, value):
+        # ensure that the value is a natural number
+        assert type(value) is int
+        assert value > 0
+
+        return self._array.__setitem__(key, value)
+
     def __repr__(self):
         return 'Natural(%s)' % repr(self._array)
 
-
-class _NaturalMatrix(_NaturalVector):
-    """Add `ℕ**M*N` interface for matrix definition. Do that by the 
-    _NaturalMatrix.__mul__() method. E.g.:
-    >>> A = Natural**3*3
-    >>> A
-    Natural([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
-    """
-
     def __mul__(self, multiplicant):
-        """Make an MxN matrix with M row an N columns where N = multiplicant
-        only if cls is an N-vector."""
+        """Add `ℕ**M*N` interface for matrix definition. Do that by the 
+        _NaturalMatrix.__mul__() method. E.g.:
+        >>> A = Natural**3*3
+        >>> A
+        Natural([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+        """
         assert self._vector is True
         assert type(multiplicant) is int
         assert multiplicant > 0
-        return _NaturalVector([self._array for j in range(0, multiplicant)])
+
+        return _NaturalMatrix([self._array for j in range(0, multiplicant)])
 
 
 class Natural(_NaturalMatrix):
