e4592200c458933ca48180940e57673008e141d5
Alan Cristhian <alan.cristh@gmail.com>
2014-10-26 16:27:20 -0300

    add test case for type checking and for get items

--- tests/test_types.py
+++ tests/test_types.py
@@ -26,6 +26,10 @@ class TestNaturalType(unittest.TestCase):
         array = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
         self.assertEqual(A._array, array)
 
+    def test_Natural_matrix_getitem(self):
+        A = Natural([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+        item = A[1][2]
+        self.assertEqual(item, 6)
 
     def test_three_dimensions_Natural_matrix_interface(self):
         A = Natural**3*3*2
@@ -34,6 +38,20 @@ class TestNaturalType(unittest.TestCase):
             [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
         ]
         self.assertEqual(A._array, array)
+
+    def test_setitem(self):
+        v = Natural**3
+        v[1] = 555
+        self.assertEqual(v._array, [0, 555, 0])
+
+    def test_error_if_set_a_non_natural_nuber(self):
+        v = Natural**3
+        with self.assertRaises(AssertionError):
+            v[0] = 3.5
+        with self.assertRaises(AssertionError):
+            v[0] = 0
+        with self.assertRaises(AssertionError):
+            v[0] = -1
 
 
 if __name__ == '__main__':
