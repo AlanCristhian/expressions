b0016d7208687c264b194406bb21c3005ce16076
Alan Cristhian <alan.cristh@gmail.com>
2014-11-04 23:31:49 -0300

    define an more accurate name

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -7,7 +7,7 @@ class Test_set_name(unittest.TestCase):
     def test_name_function(self):
         class A:
             def __init__(self):
-                self.__name__ = helpers.set_name()
+                self.__name__ = helpers.get_name()
 
         object_name = A()
         self.assertEqual(object_name.__name__, 'object_name')
