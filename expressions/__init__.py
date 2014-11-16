7d72c2c38c9d96ff1c0544b5d11a5b81bdea9443
Alan Cristhian <alan.cristh@gmail.com>
2014-11-16 15:27:14 -0300

    add System class for equalities systems

--- expressions/__init__.py
+++ expressions/__init__.py
@@ -9,3 +9,13 @@ class Object(core.BaseType):
 
     def __repr__(self):
         return 'Object(%s)' % repr(self._array)
+
+
+class System(core.BaseType):
+    """A class that wrap a generator that have an system of
+    equalities."""
+    def __setitem__(self, key, value):
+        return self._array.__setitem__(key, value)
+
+    def __repr__(self):
+        return 'System(%s)' % repr(self._array)
