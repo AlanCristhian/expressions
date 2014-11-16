22f200b5f0e1263dd7061c0a793380b54001c531
Alan Cristhian <alan.cristh@gmail.com>
2014-11-16 15:30:15 -0300

    remove the System class and refactor the solve function0

--- expressions/solvers.py
+++ expressions/solvers.py
@@ -1,17 +1,18 @@
 from expressions import core
 
 
-class System(core.BaseType):
-    """The most generic type."""
-    def __setitem__(self, key, value):
-        return self._array.__setitem__(key, value)
-
-    def __repr__(self):
-        return 'System(%s)' % repr(self._array)
+def solve_single_equality(equality, variable):
+    expression = equality.replace("==","-(") + ")"
+    c = eval(expression, {variable: 1j})
+    return {variable: -c.real/c.imag}
 
 
 def solve(system):
     variable = system._generator.gi_code.co_varnames[1:][0]
-    expression = system._expression.replace("==","-(") + ")"
-    c = eval(expression, {variable: 1j})
-    return {variable: -c.real/c.imag}
+    return solve_single_equality(system._expression, variable)
+
+
+def extract_coefficients(system):
+    print(system._expression)
+    # equalities = system._expression.split('|')
+    # equality 
