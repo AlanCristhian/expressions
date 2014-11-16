ee3c47e08f6ffea8dc038106eda783e934d21bde
Alan Cristhian <alan.cristh@gmail.com>
2014-11-16 15:28:54 -0300

    first test case

--- tests/test_expressions.py
+++ tests/test_expressions.py
@@ -1,10 +1,15 @@
 import unittest
 
-import expressions
+import expressions as ex
 
 
-class TestOject(unittest.TestCase):
-    pass
+class TestNatural(unittest.TestCase):
+    @unittest.skip('Not yet implemented')
+    def test_natural_argument(self):
+        function = ex.Object(x*2 for x in ex.Natural)
+        value = function(2)
+        result = next(value)
+
 
 
 if __name__ == '__main__':
