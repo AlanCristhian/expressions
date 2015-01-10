0f0578c6606428bf304d3bfae64adcc7f5193aee
Alan Cristhian <alan.cristh@gmail.com>
2015-01-10 14:06:56 -0300

    the implementation of the solve function is done

--- symbolic/solvers.py
+++ symbolic/solvers.py
@@ -18,7 +18,7 @@ def extract_independent_term(expression, varnames):
     return eval(expression)
 
 
-def extract_coefficients(system):
+def coefficients_matrix(system):
     equalities = system._expression.split('|')
     expression_list = [to_expression(equality) for equality in equalities]
     varnames = system._generator.gi_code.co_varnames[1:] # variabe name list
@@ -32,7 +32,8 @@ def extract_coefficients(system):
         extract_independent_term(e, varnames) for e in expression_list]
 
     coeff_column_expr = []
-    for expression, independent_term in zip(expression_list, indepentent_terms):
+    b = []
+    for expression,independent_term in zip(expression_list, indepentent_terms):
         coeff_row_expr = []
         for row in identity_matrix:
             coeff_expr = expression
@@ -45,43 +46,56 @@ def extract_coefficients(system):
     coefficient_matrix = [
         [eval(item) for item in row]
         for row in coeff_column_expr]
+
     return coefficient_matrix
 
 
-def to_upper_triangular(matrix):
-    N = len(matrix)
-    for j in range(N): # for each column on the main diag
-        if matrix[j][j] == 0: # Find a non-zero pivot and swap rows 
-            column = [matrix[k][j] for k in range(j, N)]
-            ipivot = column.index(max(column))
-            temp = matrix[j]
-            matrix[j] = matrix[ipivot]
-            matrix[ipivot] = temp
-        for i in range(j + 1, N):
-            # Ratio of (i,j) elt by (j,j) (diagonal) elt
-            c = matrix[i][j]/matrix[j][j] 
-            matrix[i] = \
-                [matrix[i][k] - c*matrix[j][k] for k in range(N)]
-    return matrix
+def gauss(A):
+    n = len(A)
+
+    for i in range(0, n):
+        # Search for maximum in this column
+        maxEl = abs(A[i][i])
+        maxRow = i
+        for k in range(i+1, n):
+            if abs(A[k][i]) > maxEl:
+                maxEl = abs(A[k][i])
+                maxRow = k
+
+        # Swap maximum row with current row (column by column)
+        for k in range(i, n+1):
+            tmp = A[maxRow][k]
+            A[maxRow][k] = A[i][k]
+            A[i][k] = tmp
+
+        # Make all rows below this one 0 in current column
+        for k in range(i+1, n):
+            c = -A[k][i]/A[i][i]
+            for j in range(i, n+1):
+                if i == j:
+                    A[k][j] = 0
+                else:
+                    A[k][j] += c * A[i][j]
+
+    # Solve equation Ax=b for an upper triangular matrix A
+    x = [0 for i in range(n)]
+    for i in range(n-1, -1, -1):
+        x[i] = A[i][n]/A[i][i]
+        for k in range(i-1, -1, -1):
+            A[k][n] -= A[k][i] * x[i]
+    return x
 
 
 def solve(system):
     # get name of each variables
-    var_name = system._generator.gi_code.co_varnames[1:][0]
-    coefficient_matrix = extract_coefficients(system)
-    T = to_upper_triangular(coefficient_matrix)
-    x = [core.ExpressionString(i) for i in 
-        system._generator.gi_code.co_varnames[1:]]
-    x.append(1)
-    w = [sum(i*j for i, j in zip(row, x) if i != 0) for row in T]
-    i = len(w)-2
-    result = {}
-    r =  [var_name] if type(var_name) is str else var_name[:]
-    r[i] = solve_single_equality(w[i]._expression, var_name[i])[var_name[i]]
-    i -= 1
-    while i >= 0:
-        expr = w[i]
-        number = solve_single_equality(expr, var_name[i])[var_name[i]]
-        result.update({var_name[i]: number})
-        i -= 1
+    var_name = system._generator.gi_code.co_varnames[1:]
+
+    # A is the coeficcient matrix and b is the vector of independent terms
+    # Ax = b
+    M = coefficients_matrix(system)
+
+    x = gauss(M)
+
+    result = {name: value for name, value in zip(var_name, x)}
+
     return result
--- test/test_solvers.py
+++ test/test_solvers.py
@@ -11,21 +11,19 @@ class TestCoefficientMatrix(unittest.TestCase):
         expected = 12
         self.assertEqual(obtained, expected)
 
-    def test_coefficient_matrix(self):
+    def test_coefficients_matrix(self):
         system = sm.System([
-            -x1  + 2*x2  - 3*x3 == 4,
-            5*x1 - 6*x2  + 7*x3  == -8,
+             -x1  + 2*x2  - 3*x3 ==  4,
+            5*x1 -  6*x2  + 7*x3 == -8,
             9*x1 + 10*x2 - 11*x3 == 12]
                 for x1, x2, x3 in sm.Any)
 
-        expected = [
-            [-1, 2, -3, 4],
-            [5, -6, 7, -8],
-            [9, 10, -11, 12]]
+        expected = [[-1,  2,  -3,  4],
+                    [ 5, -6,   7, -8],
+                    [ 9, 10, -11, 12]]
 
-        obtained = solvers.extract_coefficients(system)
+        obtained = solvers.coefficients_matrix(system)
         self.assertEqual(obtained, expected)
-
 
 class TestSolver(unittest.TestCase):
     def test_solve_single_variable_linear_equation(self):
@@ -34,7 +32,7 @@ class TestSolver(unittest.TestCase):
         result = solvers.solve(expr)
         self.assertEqual(result, {'x': 3236.0})
 
-    def test_cero_equation(self):
+    def test_zero_equation(self):
         expr = sm.System(
             0 == x + 4 for x in sm.Any)
         result = solvers.solve(expr)
@@ -47,9 +45,10 @@ class TestSolver(unittest.TestCase):
             9*x1 + 10*x2 - 11*x3 == 12]
                 for x1, x2, x3 in sm.Any)
         r = solvers.solve(system)
-        self.assertAlmostEqual(r['x1'], 0)
-        self.assertAlmostEqual(r['x2'], -1)
-        self.assertAlmostEqual(r['x3'], -2)
+        self.assertAlmostEqual(r['x1'], 0.0)
+        self.assertAlmostEqual(r['x2'], -1.0)
+        self.assertAlmostEqual(r['x3'], -2.0)
+
 
 if __name__ == '__main__':
     unittest.main()
